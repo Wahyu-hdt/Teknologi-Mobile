6ef4d94e2fbce5705551e1a6b00c003bd6649205
wahyu-hdt <wahyuhardiyantaraa@gmail.com>
2025-04-29 22:05:49 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String result = "Loading...";
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/users/1');
+    final response = await http.get(url);
+    await Future.delayed(Duration(seconds: 1));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        result = data['name'];
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        result = "Failed to load data";
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(
+            child: Text(
+              "Flutter App Example",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+
+        body: Center(
+          child:
+              isLoading
+                  ? CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      const Color.fromARGB(255, 98, 110, 116),
+                    ),
+                    strokeWidth: 3,
+                    backgroundColor: const Color.fromARGB(255, 245, 238, 221),
+                  )
+                  : Card(
+                    elevation: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.all(32),
+
+                      child: Text(
+                        result,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w500,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+        ),
+      ),
+    );
+  }
+}
