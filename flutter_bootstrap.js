94e76f9c94ec84934c588bad67878cfc5434cecd
wahyu-hdt <wahyuhardiyantaraa@gmail.com>
2025-04-30 17:21:09 +0700

    web build test 1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3371471904"
+    serviceWorkerVersion: "3865540609"
   }
 });
